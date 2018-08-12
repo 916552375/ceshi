f7c3105797ed5e6ea8c680d4f4414eeb96912685
916552375 <916552375@qq.com>
2018-08-12 09:57:48 +0800

    "Add a file named Ceshi.java"

---
+++ src/Ceshi.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author Administrator
+ *
+ */
+public class Ceshi {
+
+	/**
+	 * 
+	 */
+	public Ceshi() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("add a file");
+	}
+
+}
